892fb2cc84272e39517742595874b846c57d1429
Sergey Zelenov <mail@zelenov.su>
2019-07-16 22:35:42 +0200

    rm

--- 6-module/1-task/src/models/Product.js
+++ 6-module/1-task/src/models/Product.js
@@ -2,12 +2,6 @@ const mongoose = require('mongoose');
 const connection = require('../libs/connection');
 
 const productSchema = new mongoose.Schema({
-  
-  
-  images: [{
-    type: String,
-  }],
-  
 });
 
 module.exports = connection.model('Product', productSchema);
