6fd559b35b2c047b53cdfda21fb1779297f071d3
RidgeA <antongriadchenko@gmail.com>
2019-08-08 23:35:34 +0300

    Send mail after an order placement

--- 8-module/2-task/solution/controllers/orders.js
+++ 8-module/2-task/solution/controllers/orders.js
@@ -1,5 +1,6 @@
 const Order = require('../models/Order');
 const mapOrder = require('../mappers/order');
+const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
   const order = await Order.create({
@@ -7,6 +8,15 @@ module.exports.checkout = async function checkout(ctx, next) {
     product: ctx.request.body.product,
     phone: ctx.request.body.phone,
     address: ctx.request.body.address,
+  });
+
+  await order.populate('product').execPopulate();
+
+  await sendMail({
+    template: 'order-confirmation',
+    locals: order,
+    to: ctx.user.email,
+    subject: 'Подтверждение создания заказа',
   });
 
   ctx.body = {order: order.id};
