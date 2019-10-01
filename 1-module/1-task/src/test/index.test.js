592c407f9d090e910883b683d30ac54413de2b1e
RidgeA <antongriadchenko@gmail.com>
2019-10-01 10:29:24 +0300

    Replace CRLF|CR|LF with os-specific line separator

--- 1-module/1-task/src/test/index.test.js
+++ 1-module/1-task/src/test/index.test.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {EOL} = require('os');
 const {execSync} = require('child_process');
 const path = require('path');
 const expect = require('chai').expect;
@@ -16,7 +17,7 @@ describe('1-module-1-task', () => {
           {
             encoding: 'utf-8',
           }
-      );
+      ).replace(/(\r\n)|(\r)/g, EOL);
 
       const output = execSync(`node ${path.join(__dirname, '../index.js')}`, {
         encoding: 'utf-8',
